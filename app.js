1dfdd2b0c5e1b922b14fb2ed6cd23d8029423ecc
brandolin <brandonacosta980@gmail.com>
2022-12-12 14:02:41 -0300

    desafio

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-console.log("hola mundo");
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+const server = app.listen(PORT, () => {
+
+  console.log(`Servidor escuchando ${server.address().port}`)
+})
+
+app.get("/productos", (req, res) => {
+  res.json(productos)
+})
+
+app.get("/productosRandom", (req, res) => {
+  let objeto = productos.id;
+  res.send(console.log(objeto))
+  
+  if (objeto == 1) {
+    res.json(productos[1])
+    
+  } else {
+    res.json(productos[0])
+    
+  }
+
+})
+server.on("error", error => console.log("error de sv"))
+
+let productos = [];
+
+productos.push({
+  nombre: "Buzo",
+  id: 1,
+  precio: "600",
+  image: ""
+},
+  {
+    nombre: "Buzo",
+    id: 2,
+    precio: "600",
+    image: ""
+  })
+
+
